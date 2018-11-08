bdcbd5f3bc544947b540dcf1245549aac86f51ce
ddshelt <ddshelton0001@gmail.com>
2018-11-08 09:57:16 -0500

    Initial commit

---
+++ blumblr/database/migrations/2018_11_08_130151_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            //name of post
+            $table->string('name', 250);
+            //unique title
+            $table->string('title')->unique();
+            $table->string('post', 500);
+            //set up user id as foreign key
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ blumblr/resources/views/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.app')
+
+@section('content')
+
+These are my posts
+
+@endsection
